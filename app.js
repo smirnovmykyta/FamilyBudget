d94ce5797aa2a6094de1f4252bea2a7f7e539b93
Никита Цикаленко <nikita.tsikalenko@gmail.com>
2022-07-21 22:21:11 +0300

    connect to db

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import express from 'express'
-const PORT = process.env.DB_PORT || 4000
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const PORT = process.env.DB_PORT || 4000
 const app = express()
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+    .then(()=>{
+        console.log('DB connected');
+    })
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
