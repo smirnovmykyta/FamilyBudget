6be6033bb4b2014a9f1c7062e00584be2300b341
Nikita <nikitas@wizardsdev.com>
2022-07-21 21:57:50 +0200

    delete timestamps in the familiesScheme

--- models/family.js
+++ models/family.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const familiesSchema = new mongoose.Schema({
     name: String,
     photo: Buffer,
-},{ timestamps: true });
+});
 
 const Families = mongoose.model('families', familiesSchema);
 
